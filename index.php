146d3db7614a958df1f2a6b1aa0962910c375349
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2021-03-29 17:29:42 +0200

    Publish the correction

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
 <?php
 
+require 'src/Fighter.php';
+
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+
+$heracles = new Fighter('🧔 Héraclès', 20, 6);
+$lion = new Fighter('🦁 Lion de Némée', 11, 13);
+
+$round = 1;
+echo '<ul>';
+
+while( $heracles->isAlive() && $lion->isAlive() ) {
+    echo "<li>🕛 Round $round</li>";
+
+    $heracles->fight($lion);
+    echo "<li>" . $heracles->getName() . ' attaque ' . $lion->getName() . '.</li>';
+    echo "<li>" .$lion->getName() . ' : ' . $lion->getLife() . '💙.</li>';
+
+    if($lion->isAlive()) {
+        $lion->fight($heracles);
+        echo "<li>" .$lion->getName() . ' attaque ' . $heracles->getName() . '.</li>';
+        echo "<li>" .$heracles->getName() . ' : ' . $heracles->getLife() . '💙.</li>';
+    }
+
+    $round++;
+}
+
+echo '</ul>';
+
+if($heracles->isAlive()) {
+    echo '<p>'. $lion->getName() .' est vaincu !</p>';
+    echo '<p>'. $heracles->getName() .' a gagné !</p>';
+} else {
+    echo '<p>'. $lion->getName() .' est vaincu !</p>';
+    echo '<p>'. $heracles->getName() .' a gagné !</p>';
+}
+
+?>
+
+
--- src/Fighter.php
+++ src/Fighter.php
@@ -2,5 +2,61 @@
 
 class Fighter
 {
+    private  $name;
+    private  $strength;
+    private  $dexterity;
+    private  $life;
+
+    private const MAX_LIFE = 100;
+
+    public function __construct(string $name, int $str, int $dex)
+    {
+        $this->name = $name;
+        $this->strength = $str;
+        $this->dexterity = $dex;
+
+        $this->life = self::MAX_LIFE;
+    }
+    
+    public function fight(Fighter $opponent) 
+    {
+
+        $damage = rand(0, $this->strength);
+        $defense = rand(0, $opponent->getDexterity());
+
+        $damage -= $defense;
+
+        if($damage < 0)
+            $damage = 0;
+
+        
+        $opponent->wounded($damage);
+
+    }
+
+    public function getName() : string 
+    {
+        return $this->name;
+    }
+
+    public function getLife() : int 
+    {
+        return $this->life;
+    }
+
+    public function getDexterity() : int
+    {
+        return $this->dexterity;
+    }
+
+    public function wounded(int $damage) : void 
+    {
+        $this->life -= $damage;
+    }
+
+    public function isAlive() : bool
+    {
+        return $this->life > 0;
+    }
 
 }
